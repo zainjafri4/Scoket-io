79488f543753667ea79dcf235a0d3851b63cd760
unknown <zainjafri4@gmail.com>
2023-11-15 05:12:45 +0500

    Basic Covered

--- server.js
+++ server.js
@@ -5,23 +5,50 @@ server = http.createServer(app);
 
 const io = require('socket.io')(server)
 
-io.on('connection', (socket)=>{
+// let full = 0;
+// let cnsp = io.of('/custom-space');
+
+let users = 0;
+let roomno = 1;
+
+io.on('connection', (socket) => {
     console.log('A User Connected');
+
+    // socket.join('Room No-' + roomno);
+    // io.sockets.in('Room No-' + roomno).emit('roomConnected', 'Room No-'+roomno);
+
+    // users++;
+
+    // if(users >= 2){
+    //     roomno++
+    //     users=0;
+    // }
+
+    // users++;
+    // socket.emit("userConnected", { Message: " Hi, welcome" })    
+    // socket.broadcast.emit("userConnected", { Message: users + " user(s) are connected" })
 
     // setTimeout(()=>{
     //     socket.send("Hi, How May I help you today?")
     // },3000)
 
-    setTimeout(()=>{
-        socket.emit('firstMessage', {Message : "Hi How may I help?"})
-    },3000)
-    
+    // setTimeout(()=>{
+    //     socket.emit('firstMessage', {Message : "Hi How may I help?"})
+    // },3000)
 
-    socket.on('disconnect', ()=>{
+    // socket.on('clientSide', (data)=>{
+    //     console.log(data)
+    // })
+
+    socket.on('disconnect', () => {
         console.log('User Dissconnected')
+
+        // users--;
+        //  socket.broadcast.emit("userConnected", { Message: users + " user(s) are connected" })
+
     })
 })
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
     console.log('Server Up')
 })
